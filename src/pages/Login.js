ebf21258e0c65715071e93e14ad1734d1540a0fc
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-05-11 20:50:31 +0530

    add image for signin page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,9 @@
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import loginImage from '../assets/login-image.jpg'; // Import your image
 import { auth } from '../firebase';
-import './Login.css'; // Importing CSS styles
+import './Login.css';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -20,28 +21,37 @@ function Login() {
   };
 
   return (
-    <div className="login-page">
-      <form className="login-form" onSubmit={handleLogin}>
-        <h2>Sign In</h2>
-        <input
-          type="email"
-          placeholder="Email Address"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
+    <div className="login-container">
+      <div className="login-form-container">
+        <form className="login-form" onSubmit={handleLogin}>
+          <h2>Welcome To Movie-Explorer</h2>
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Login</button>
+          <p className="signup-link">
+            Don't have an account? <Link to="/signup">Sign Up</Link>
+          </p>
+        </form>
+      </div>
+      <div className="login-image-container">
+        <img 
+          src={loginImage} 
+          alt="Movie theater" 
+          className="login-image"
         />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Login</button>
-        <p style={{ marginTop: '10px', textAlign: 'center' }}>
-          Don't have an account? <Link to="/signup">Sign Up</Link>
-        </p>
-      </form>
+      </div>
     </div>
   );
 }
